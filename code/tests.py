81ebb63bdcf4059d100e2b31a3e1df6a6cfd23a9
Bill Merrill <bill.merrill@zapier.com>
2018-04-13 07:53:29 -0700

    Test for triangulating a rectangle.

--- code/tests.py
+++ code/tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 import cube
-from utils import *
+from utils import Point, Triangle
 
 
 class CubeTests(unittest.TestCase):
@@ -29,7 +29,23 @@ class CubeTests(unittest.TestCase):
             self.assertIn(p, expected_points)
 
     def test_rect_triangles(self):
-        self.assertFalse(True)
+        nw = Point(-20, 1, 19)
+        se = Point(18, 1, -17)
+        ne = Point(se.x, se.y, nw.z)
+        sw = Point(nw.x, nw.y, se.z)
+
+        results = cube.generate_rect_triangles(nw, ne, sw, se)
+        expected1 = Triangle(nw, sw, se)
+        expected2 = Triangle(nw, se, ne)
+        self.assertEqual(results[0], expected1)
+        self.assertEqual(results[1], expected2)
 
     def test_cube_trianges(self):
+        high = Point(-20, 16, 19)
+        low = Point(18, -15, -17)
+
+        results = cube.generate_cube_trianges(high, low)
+        self.assertEqual(len(results), )
+
+
         self.assertFalse(True)
--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,4 @@
 from collections import namedtuple
 
 Point = namedtuple('Point', ['x', 'y', 'z'])
-Triangle = namedType(Triangle, ['a', 'b', 'c'])
+Triangle = namedtuple('Triangle', ['a', 'b', 'c'])
